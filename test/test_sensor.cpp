9986b1f5fbee3e8b0a6850cbecb51011952bc5ac
Ivan Cerjan <ivan.cerjan@king-ict.cloud>
2026-09-18 23:10:46 +0200

    add tests, add environments, test each piece of hardware and wiring

---
+++ test/test_sensor.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+
+    Wire.begin(21, 22); // SDA, SCL
+
+    if (!mlx.begin()) {
+        Serial.println("Can't find MLX90614 sensor. Check wires!");
+        while (1) {
+            delay(1000);
+        }
+    }
+
+    Serial.println("MLX90614 sensor ready.");
+}
+
+void loop() {
+    float objectTemp = mlx.readObjectTempC();
+    float ambientTemp = mlx.readAmbientTempC();
+
+    Serial.print("Food temp: ");
+    Serial.print(objectTemp);
+    Serial.print(" °C | Ambient: ");
+    Serial.print(ambientTemp);
+    Serial.println(" °C");
+
+    delay(1000);
+}
